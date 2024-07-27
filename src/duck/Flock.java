f7bd2a85a45ec3596321762a2df797abcecd4542
Harit Sombatsiri <haritsombatsiri@gmail.com>
2024-07-27 12:57:55 +0700

    first commit

---
+++ src/duck/Flock.java
@@ -0,0 +1,19 @@
+package duck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Flock implements Quackable {
+    List<Quackable> quackers = new ArrayList<>();
+
+    public void add(Quackable quackable) {
+        quackers.add(quackable);
+    }
+
+    @Override
+    public void quack() {
+        System.out.println("");
+        for (Quackable q : quackers)
+            q.quack();
+    }
+}
